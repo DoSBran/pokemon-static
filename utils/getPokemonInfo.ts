1a7a2b3bddff23003baec53c6bdc18113662f259
brandoncfridivi <96086960+brandoncfridivi@users.noreply.github.com>
2023-05-13 13:01:00 -0600

    incremental static generation

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,6 +2,7 @@ import { pokeAPI } from "@/api";
 import { PokemonDetail } from "@/interfaces";
 
 export const getPokemonInfo = async(nameOrId: string) => {
+  try {
     const { data } = await pokeAPI.get<PokemonDetail>(`/pokemon/${nameOrId}`);
     const pokemon = {
       id: data.id,
@@ -9,4 +10,8 @@ export const getPokemonInfo = async(nameOrId: string) => {
       sprites: data.sprites,
     };
     return pokemon;
+  } catch (error) {
+    return null;
+  }
+    
 }
